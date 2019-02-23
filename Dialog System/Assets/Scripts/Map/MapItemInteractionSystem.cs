786e55fc1636416dd080510774b9e1dc531905ef
TrollTier <patricksicking@web.de>
2019-02-23 16:05:36 +0100

    Map item system included

---
+++ Dialog System/Assets/Scripts/Map/MapItemInteractionSystem.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity.Entities;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts.Map
+{
+    [UpdateAfter(typeof(MapItemsSystem))]
+    class MapItemInteractionSystem : ComponentSystem
+    {
+
+#pragma warning disable CS0649
+        struct Data
+        {
+            public readonly int Length;
+            public ComponentArray<MapItem> Items;
+        }
+#pragma warning restore CS06449
+
+        [Inject] private Data data;
+
+        protected override void OnUpdate()
+        {
+            if (data.Length == 0 || !(Input.GetButton("Fire1"))) return;
+
+            MapItem selectedItem = null;
+            for (int i = 0; i < data.Length && selectedItem == null; i++)
+            {
+                var item = data.Items[i];
+                if (item.isSelected)
+                {
+                    selectedItem = item;
+                }
+            }
+
+            if (!string.IsNullOrWhiteSpace(selectedItem?.SceneName))
+            {
+                SceneManager.LoadScene(selectedItem.SceneName);
+            }
+        }
+    }
+}
