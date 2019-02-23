a1833c282afc4c067efa8d95e3a5e6d8cd0780fe
TrollTier <patricksicking@web.de>
2019-02-23 17:32:33 +0100

    Maps, dummy:   -> Map liegt jetzt auf eigenem Sort-Layer   -> Dummy char für Tests hinzugefügt

--- Dialog System/Assets/Scripts/Player/PlayerData.cs
+++ Dialog System/Assets/Scripts/Player/PlayerData.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace Assets.Scripts.Player
 {
-    public class Player
+    public class PlayerData : MonoBehaviour
     {
-        public float MoveSpeed = 1;
+        public float MoveSpeed = 2;
+        public float SprintSpeed = 3;
     }
 }
---
+++ Dialog System/Assets/Scripts/Player/PlayerInputSystem.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity.Entities;
+using UnityEngine;
+
+namespace Assets.Scripts.Player
+{
+    public class PlayerInputSystem : ComponentSystem
+    {
+        private struct Entities
+        {
+            public readonly int Length;
+            public ComponentArray<Rigidbody2D> Bodies;
+            public ComponentArray<PlayerData> Players;
+        }
+
+        [Inject] private Entities data;
+
+        protected override void OnUpdate()
+        {
+            if (data.Length == 0) return;
+
+            var horizontal = Input.GetAxis("Horizontal");
+            var vertical = Input.GetAxis("Vertical");
+            var dir = new Vector2(horizontal, vertical).normalized;
+
+            var isSprinting = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
+            for (int i = 0; i < data.Length; i++)
+            {
+                var body = data.Bodies[i];
+                var player = data.Players[i];
+
+                var movementFactor = ((isSprinting) ? player.SprintSpeed : player.MoveSpeed);
+                body.velocity = dir * movementFactor;
+            }
+        }
+    }
+}
